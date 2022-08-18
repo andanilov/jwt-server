598705d3688cba95b4526209c2f60d0d3acb048c
andanilov <andi.danilov@gmail.com>
2022-08-18 15:58:21 +0300

    fix bugs

--- service/pdf-service.js
+++ service/pdf-service.js
@@ -14,77 +14,72 @@ const ApiError = require('../exceptions/api-error');
 
 class PdfService {
   async addTableFile(tpl, fileBuffer) {
-    try {
-      // 0. Config
-      const separator = ';';
-      const maxColLen = 128;
-      const maxDataLen = 256;
-      const colNameStopList = ['tpl', 'updated'];
+    // 0. Config
+    const separator = ';';
+    const maxColLen = 128;
+    const maxDataLen = 256;
+    const colNameStopList = ['tpl', 'updated'];
 
-      // 1. Chek if no buffer
-      if (!Buffer.isBuffer(fileBuffer)) {
-        throw ApiError.BadRequest(`Недостаточно данных!`);
-      }
-
-      // 2. Get file from buffer to strings array
-      const fileStrArr = new TextDecoder('windows-1251')
-        .decode(new Uint8Array(fileBuffer))
-        .split('\r\n');
-
-      // 3. Get table header
-      const theadArr = fileStrArr.shift().split(separator);
-      if (!theadArr.includes('code')) {
-        throw ApiError.BadRequest(`Таблица не содержит обязательную колонку code!`);
-      }
-      if (new Set([...colNameStopList, ...theadArr]).size < [...colNameStopList, ...theadArr].length) {
-        throw ApiError.BadRequest(`Таблица содержит зарезервированные системой поля: ${colNameStopList.join(', ')}`);
-      }
-
-      // 4. Set table struct (col - colNumber)
-      const tableMap = theadArr.reduce((map, col, i) =>
-        (!col.trim() || col.trim().length > maxColLen)
-          ? map
-          : { ...map, [col.trim()]: i}
-      , {});
-      const codePos = tableMap.code;
-      // delete tableMap.code;
-
-      if (!Object.keys(tableMap).length) {
-        throw ApiError.BadRequest(`Недостаточно данных в таблице!`);
-      }
-
-      // 5. Add columns if not exist
-      Object.keys(tableMap).length && await db.query(Object.keys(tableMap).map((col) =>
-        `ALTER TABLE pdfdata ADD COLUMN IF NOT EXISTS ${col} VARCHAR(${maxDataLen})`)
-        .join('; '));
-
-      // 6. Add params to DB
-      let rows = 0;
-      fileStrArr.forEach(async (data) => {
-        const dataArr = data.split(separator);
-
-        // Check if no code
-        if (!dataArr[codePos] || dataArr[codePos].trim() === '') {
-          return;
-        }
-
-        dataArr[codePos] = dataArr[codePos].replace(/ /g, '');
-        const dataStrByMap = Object.values(tableMap).reduce((table, col) => [...table, dataArr[col]], []);
-
-        await db.query(`INSERT INTO pdfdata 
-          (tpl, updated, ${Object.keys(tableMap).join(', ')})
-          VALUES ($1, current_timestamp, ${dataArr.map((_, key) => '$' + (key + 2))})
-          ON CONFLICT (code) DO UPDATE SET
-          updated=current_timestamp ${Object.keys(tableMap).map((col, key) => col === 'code' ? '' : ` ${col}=$${key + 2}`)} ;`,
-          [tpl, ...dataStrByMap]);
-
-        rows += 1;
-      });
-      
-      return rows;
-    } catch (e) {
-      throw ApiError.BadRequest(`Не удалось обновить данные по файлу-таблице: ${e.message}`);
+    // 1. Chek if no buffer
+    if (!Buffer.isBuffer(fileBuffer)) {
+      throw ApiError.BadRequest(`Недостаточно данных!`);
     }
+
+    // 2. Get file from buffer to strings array
+    const fileStrArr = new TextDecoder('windows-1251')
+      .decode(new Uint8Array(fileBuffer))
+      .split('\r\n');
+
+    // 3. Get table header
+    const theadArr = fileStrArr.shift().split(separator);
+    if (!theadArr.includes('code')) {
+      throw ApiError.BadRequest(`Таблица не содержит обязательную колонку code!`);
+    }
+    if (new Set([...colNameStopList, ...theadArr]).size < [...colNameStopList, ...theadArr].length) {
+      throw ApiError.BadRequest(`Таблица содержит зарезервированные системой поля: ${colNameStopList.join(', ')}`);
+    }
+
+    // 4. Set table struct (col - colNumber)
+    const tableMap = theadArr.reduce((map, col, i) =>
+      (!col.trim() || col.trim().length > maxColLen)
+        ? map
+        : { ...map, [col.trim()]: i}
+    , {});
+    const codePos = tableMap.code;
+    // delete tableMap.code;
+
+    if (!Object.keys(tableMap).length) {
+      throw ApiError.BadRequest(`Недостаточно данных в таблице!`);
+    }
+
+    // 5. Add columns if not exist
+    Object.keys(tableMap).length && await db.query(Object.keys(tableMap).map((col) =>
+      `ALTER TABLE pdfdata ADD COLUMN IF NOT EXISTS ${col} VARCHAR(${maxDataLen})`)
+      .join('; '));
+
+    // 6. Add params to DB
+    let rows = 0;
+    fileStrArr.forEach(async (data) => {
+      const dataArr = data.split(separator);
+
+      // Check if no code
+      if (!dataArr[codePos] || dataArr[codePos].trim() === '') {
+        return;
+      }
+
+      dataArr[codePos] = dataArr[codePos].replace(/ /g, '');
+      const dataStrByMap = Object.values(tableMap).reduce((table, col) => [...table, dataArr[col]], []);
+
+      rows += 1;
+      await db.query(`INSERT INTO pdfdata 
+        (tpl, updated, ${Object.keys(tableMap).join(', ')})
+        VALUES ($1, current_timestamp, ${dataArr.map((_, key) => '$' + (key + 2))})
+        ON CONFLICT (code) DO UPDATE SET
+        updated=current_timestamp ${Object.keys(tableMap).map((col, key) => col === 'code' ? '' : ` ${col}=$${key + 2}`)} ;`,
+        [tpl, ...dataStrByMap]);
+    }); 
+         
+    return rows;
   }
 
   async getProductByCode(code) {
